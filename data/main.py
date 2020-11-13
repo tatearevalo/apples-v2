f62ab3a5fd5731a745c93b2ce8059d1dc6cb1051
Jimmy Herman <jamesher@andrew.cmu.edu>
2020-11-12 20:17:59 -0500

    consolidated files

--- data/main.py
+++ data/main.py
@@ -4,10 +4,11 @@
 #                                                                             #
 #  main.py                                                                    #
 #                                                                             #
-#    Scrapes Apples-to-Apples cards from the web                              #
+#    Scrapes Apples-to-Apples cards from the web and dumps to csv             #
 #                                                                             #
 ###############################################################################
 
+import csv
 import json
 
 import bs4
@@ -16,7 +17,30 @@ from scrape import AppleScraper
 
 RED_CARDS_URL = 'http://www.com-www.com/applestoapples/applestoapples-red-with.html'
 GREEN_CARDS_URL = 'http://www.com-www.com/applestoapples/applestoapples-green-with.html'
-OUTPUT_FILENAME = 'cards.json'
+
+RED_CSV='red_cards.csv'
+GREEN_CSV='green_cards.csv'
+
+FIELDS = [
+	'id',
+	'item',
+	'description',
+	'party_set',
+	'basic_set',
+	'junior_set',
+	'expansion_set_1',
+	'expansion_set_2',
+	'expansion_set_3',
+	'expansion_set_4',
+	'party_expansion_set'
+]
+
+def write_to_csv(filename, cards):
+	w = csv.writer(open(filename))
+	for idx, card in enumerate(cards):
+		row = [str(idx)] + [str(card[f]) for f in FIELDS[1:]]
+		cleaned_row = [field.replace(',','') for field in row]
+		w.writerow(cleaned_row)
 
 if __name__=='__main__':
 
@@ -25,12 +49,13 @@ if __name__=='__main__':
 		green_url=GREEN_CARDS_URL
 	)
 
-	output = {
-		'red_cards': apple_scraper.scrape(apples='red'),
-		'green_cards': apple_scraper.scrape(apples='green')
-	}
+	# scrape into json/dict
+	red_cards = apple_scraper.scrape(apples='red'),
+	green_cards = apple_scraper.scrape(apples='green')
 
-	with open(OUTPUT_FILENAME, 'w') as f:
-		json.dump(output, f)
+	# dump output
+	write_to_csv(RED_CSV, red_cards)
+	write_to_csv(GREEN_CSV, green_cards)
 
+	print('Complete')
 	
